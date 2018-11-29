e3ac01a628f67fadbfdcc29a7f4ca1e4060f8006
Suresh Korlapati <sureshkorlapati@gmail.com>
2018-11-29 15:55:24 -0600

    Add socket.io, connect and disconnect serverside and client side code

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# node-chat-app. steps to add the app to github
+# node-chat-app. 
+# steps to add the app to github
 echo "# node-chat-app" >> README.md
 git status
 git add .
@@ -7,4 +8,9 @@ git remote add origin https://github.com/sureshkorlapati/node-chat-app.git
 git push -u origin master
 
 
-#
+# install heroku cli
+sudo apt install snapd -y
+sudo snap install --classic heroku -y
+sudo heroku create
+sudo heroku login
+sudo git push heroku master
--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const sockerIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+var io = sockerIO(server);
+
 app.use(express.static(publicPath));
+
+io.on('connection',(socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
 
 
 console.log(publicPath);
 
-app.listen(port,() => {
+server.listen(port,() => {
     console.log(`server is up on port ${port}`);
 });
